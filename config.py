56621304f26dbb24fe9ef8ca131d1581ee0a7251
Lord Divan <darveter667@gmail.com>
2023-08-27 17:36:56 +0300

    init

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+DB_HOST: str = os.environ.get("DB_HOST", "172.17.0.3")
+DB_PORT: str = os.environ.get("DB_PORT", "5432")
+DB_NAME: str = os.environ.get("DB_NAME", "postgres")
+DB_USER: str = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD: str = os.environ.get("DB_PASSWORD", "postgres")
