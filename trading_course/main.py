b72af789fc0742173a32e43341196335054d0324
Lord Divan <darveter667@gmail.com>
2023-08-31 09:41:20 +0300

    docker-compose alembic was added, little refactoring, Dockefile was added, requirements.txt were added

--- trading_course/main.py
+++ trading_course/main.py
@@ -12,4 +12,4 @@ app = FastAPI(docs_url="/api/docs")
 app.include_router(main_router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="web_app", port=8000)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
